d2e61a3a1ccd41d224a49829b156a8aaaf015ee4
Arthur Felipe Dioses Reto <adiosesr@everis.com>
2018-11-27 14:07:57 -0500

    -Insertando databinding en los elementos android

--- app/src/main/java/com/example/everis/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/everis/kotlinintro/MainActivity.kt
@@ -5,30 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.everis.kotlinintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnDone).setOnClickListener {
+        /* findViewById<Button>(R.id.btnDone).setOnClickListener {
+             addNickname(it)
+         }
+         */
+        binding.btnDone.setOnClickListener {
             addNickname(it)
         }
-
     }
 
     private fun addNickname(view: View) {
-        val etNickname = findViewById<EditText>(R.id.etNickName)
-        val tvNickname = findViewById<TextView>(R.id.tvNickName)
-
+        //val etNickname = findViewById<EditText>(R.id.etNickName)
+        //val tvNickname = findViewById<TextView>(R.id.tvNickName)
+        binding.apply {
+            binding.tvNickName.text = binding.etNickName.text
+            binding.etNickName.visibility = View.GONE
+            binding.btnDone.visibility = View.GONE
+            binding.tvNickName.visibility = View.VISIBLE
+        }
+        /*
         tvNickname.text = etNickname.text
         etNickname.visibility = View.GONE
         view.visibility = View.GONE
         tvNickname.visibility = View.VISIBLE
+        */
+
 
         //Ocultamos el teclado
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
